d5607fc94deeefb1582ed0b58f696c8ae40e8ca9
KUMARA SWAMY KANCHARI <63644841+Kanchari-Kumara-Swamy@users.noreply.github.com>
2020-05-20 15:48:01 +0530

    Update REST APIs project.py

--- REST APIs project.py
+++ REST APIs project.py
@@ -1,4 +1,4 @@
-#This code might not be reusable because you can find any "request_with_caching" module in python but can you can develop your own,
+#This code might not be reusable because you can't find any "request_with_caching" module in python but can you can develop your own,
 #the purpose is understand how api works and in this code is written to extract movies names, if you want to write query on other things 
 # you can visit the api file provided and give the required parameters in param attribute in requests_with_caching.get function.
 
