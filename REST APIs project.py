bba9363e1bd21ebf78f739a87ef0471564fa88d3
KUMARA SWAMY KANCHARI <63644841+Kanchari-Kumara-Swamy@users.noreply.github.com>
2020-07-11 20:07:38 +0530

    Update REST APIs project.py

--- REST APIs project.py
+++ REST APIs project.py
@@ -1,10 +1,10 @@
-#This code might not be reusable because you can't find any "request_with_caching" module in python but can you can develop your own,
-#the purpose is understand how api works and in this code is written to extract movies names, if you want to write query on other things 
-# you can visit the api file provided and give the required parameters in param attribute in requests_with_caching.get function.
+'''This project will take you through the process of mashing up data from two different APIs to make movie recommendations.
+You will put those two together. You will use TasteDive to get related movies for a whole list of titles.
+You’ll combine the resulting lists of related movies, and sort them according to their Rotten Tomatoes scores'''
 
-
-import requests_with_caching
+import requests
 import json
+
 
 def get_movies_from_tastedive(string):
     baseurl = "https://tastedive.com/api/similar"
@@ -12,13 +12,10 @@ def get_movies_from_tastedive(string):
     name["q"] = string
     name["type"] = "movies"
     name["limit"] = 5
-    final = requests_with_caching.get(baseurl, params = name)
+    final = requests.get(baseurl, params=name)
     print(final.url)
-    x = json.loads(final.text, indent = 4)
-    return(x)
-	
-
-print("----------------------------")  
+    x = json.loads(final.text)
+    return (x)
 
 
 def extract_movie_titles(file):
@@ -27,10 +24,10 @@ def extract_movie_titles(file):
         result.append(x['Name'])
     return result
 
-print("----------------------------")  
+
 def get_related_titles(movies):
-    if movies!= []:
-        result =[]
+    if movies != []:
+        result = []
         for movie in movies:
             res = extract_movie_titles(get_movies_from_tastedive(movie))
             [result.append(x) for x in res if x not in result]
@@ -38,39 +35,35 @@ def get_related_titles(movies):
     else:
         return movies
 
-print("----------------------------")  
-
-
-
 def get_movie_data(string):
     baseurl = "http://www.omdbapi.com/"
     name = {}
+    name['apikey'] = "8edc265e"
     name['t'] = string
     name['r'] = 'json'
-    final = requests_with_caching.get(baseurl , params = name)
+    final = requests.get(baseurl, params=name)
     print(final.url)
     x = final.json()
     return x
 
-print("----------------------------") 
+
 
 def get_movie_rating(file):
-    val= ''
+    val = ''
     for x in file['Ratings']:
         if x['Source'] == 'Rotten Tomatoes':
             val = x['Value']
     if val != '':
-        rating = int(val[:2])
+        rating = val[:3]
     else:
         rating = 0
     return rating
-    
-print("----------------------------") 
+
+
 
 def get_sorted_recommendations(titles):
     elements = get_related_titles(titles)
-    title = sorted(elements, key = lambda element: (get_movie_rating(get_movie_data(element)), element), reverse = True)
+    title = sorted(elements, key=lambda element: (get_movie_rating(get_movie_data(element)), element), reverse=True)
     return title
 # You can call the function as below by passing arguments you like.
-#print(get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"])) 
-    
+print(get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"]))
